7d5b090bf54869a0b7e6189c34c4415ad5349542
Itsssammara <hoosenammara@gmail.com>
2023-12-08 15:23:06 +0200

    edited products

--- SCRIPT/product.js
+++ SCRIPT/product.js
@@ -1,69 +1,3 @@
-// let goods =[]
-// let main = document.querySelector('main')
-
-// // sets the array from local storage to array(items) in code
-// art= JSON.parse(localStorage.getItem('art'))
-
-// main.innerHTML = items.map(function(item, index){
-//     return `
-//     <div>
-//         <h2>${item.name}</h2>
-//         <p><img src="${item.url}"></img></p>
-//         <p>${item.description}</p>
-//         <p>${item.price}</p>
-//         <button value="${index}" data-add>Add to cart</button>
-//     </div>`
-// }).join('')
-
-// function add(index){
-//     purchased.push(items[index])
-//     localStorage.setItem('goods',JSON.stringify(purchased))
-// }
-// main.addEventListener('click', function(){
-//     if(event.target.hasAttribute('data-add')){
-//         // alert('button pressed')
-//         add(event.target.value)
-// }
-// })
-
-// //adding products to the shopping cart
-// let cart = []
-
-// document.getElementById("digitalBtn").addEventListener("click", addToCart);
-// document.getElementById("postBtn").addEventListener("click", addToCart);
-// document.getElementById("mediaBtn").addEventListener("click", addToCart);
-// document.getElementById("oilBtn").addEventListener("click", addToCart);
-// document.getElementById("polaroidBtn").addEventListener("click", addToCart);
-// document.getElementById("canvasBtn").addEventListener("click", addToCart);
-
-// function addToCart(artpiece) {
-//     cart.push(artpiece); 
-//     localStorage.setItem('shoppingCart', JSON.stringify(cart));
-//   }
-  
-//   function addProductToCart(productName, productPrice) {
-//     //adds the product to an array 
-//     console.log("Added " + productName + " to cart. Price: R" + productPrice);
-//   }
-
-//   function searchAndSort() {
-//     let searchBar = document.getElementById('searchBar').value.toLowerCase();
-//     let sortOption = document.getElementById('sortOptions').value;
-
-//     let filteredItems = accessories.filter(item => {
-//         return item.name.toLowerCase().includes(searchBar);
-//     });
-
-//     if (sortOption === 'cheapest') {
-//         filteredItems.sort((a, b) => a.price - b.price);
-//     } else if (sortOption === 'expensive') {
-//         filteredItems.sort((a, b) => b.price - a.price);
-//     }
-
-//     renderItems(filteredItems);
-// }
-
-
 let goods = []
 let main = document.querySelector('main')
 
@@ -112,24 +46,19 @@ function addToCart(artpiece) {
 function addProductToCart(productName, productPrice) {
     console.log("Added " + productName + " to cart. Price: R" + productPrice);
 }
-
-function searchAndSort() {
+//function runs items you search 
+function search() {
     let searchBar = document.getElementById('searchBar').value.toLowerCase();
-    let sortOption = document.getElementById('sortOptions').value;
-
     let filteredItems = goods.filter(item => {
         return item.name.toLowerCase().includes(searchBar);
     });
-
-    if (sortOption === 'lowest price') {
-        filteredItems.sort((a, b) => a.price - b.price);
-    } else if (sortOption === 'highest price') {
-        filteredItems.sort((a, b) => b.price - a.price);
-    }
-
-    renderItems(filteredItems);
+    addToCart(filter);
 }
-
+document.getElementById('input').addEventListener('input', search);
+//sorts prices from high to low
+function sort(){
+    let sort = document.getElementById('select').value;
+}
 
 
 
